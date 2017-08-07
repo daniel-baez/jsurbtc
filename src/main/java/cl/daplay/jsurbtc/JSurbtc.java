1277ea8a5fd31a94d91ad1b74434dac4882b87a2
daplay <daplay@gmail.com>
2017-08-06 20:50:50 -0700

    sets UTF-8 for file encoding

--- src/main/java/cl/daplay/jsurbtc/JSurbtc.java
+++ src/main/java/cl/daplay/jsurbtc/JSurbtc.java
@@ -122,8 +122,8 @@ public final class JSurbtc {
 
     public List<Order> getOrders(final MarketID marketID, final OrderState orderState) throws Exception {
         /*
-         * per	300	Numero de ordenes por página [min 1, max 300]
-         * page	1	Numero de pagina a recibir
+         * per	300	Número de ordenes por página [min 1, max 300]
+         * page	1	Número de pagina a recibir
          * state	None	Estado de la orden
          * minimun_exchanged	None	Minimo transado por la orden
          */

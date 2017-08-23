716975dacce212acfd3666bdd2300664de76fb8d
daplay <daplay@gmail.com>
2017-08-22 22:23:22 -0700

    removed `Thread.sleep(1)` from Nonce.INSTANCE

--- src/main/java/cl/daplay/jsurbtc/Nonce.java
+++ src/main/java/cl/daplay/jsurbtc/Nonce.java
@@ -2,23 +2,12 @@ package cl.daplay.jsurbtc;
 
 import java.util.function.LongSupplier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public enum Nonce implements LongSupplier {
     INSTANCE;
 
-    private static final Logger logger = LoggerFactory.getLogger(Nonce.class);
-
     @Override
     public long getAsLong() {
-        try {
-            Thread.sleep(1);
-        } catch (final InterruptedException ex) {
-            logger.warn("Thread interruption detected while calling Nonce", ex);
-        } finally {
-            return System.currentTimeMillis();
-        }
+        return System.currentTimeMillis() / 1000;
     }
     
 }

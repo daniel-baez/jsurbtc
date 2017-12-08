acaa2dae076209a63fecf4ab1a4d3b7382c345f1
daplay <daplay@gmail.com>
2017-12-08 11:33:45 -0800

    - Currency now includes a flag for Crypto currencies

--- src/main/java/cl/daplay/jsurbtc/model/Currency.java
+++ src/main/java/cl/daplay/jsurbtc/model/Currency.java
@@ -4,21 +4,34 @@ import java.math.BigDecimal;
 
 public enum Currency {
     USD(new BigDecimal("100")),
-    BTC(new BigDecimal(1e8)),
-    BCH(new BigDecimal(1e8)),
+    BTC(new BigDecimal(1e8), true),
+    BCH(new BigDecimal(1e8), true),
     CLP(new BigDecimal("100")),
     COP(new BigDecimal("100")),
-    ETH(new BigDecimal(1e9)),
+    ETH(new BigDecimal(1e9), true),
     PEN(new BigDecimal("100")),
     CNY(new BigDecimal("100"));
 
     private final BigDecimal subunitsToUnit;
+    private final boolean isCrypto;
 
     Currency(final BigDecimal subunitsToUnit) {
+        this(subunitsToUnit, false);
+    }
+
+    Currency(final BigDecimal subunitsToUnit, final boolean isCrypto) {
         this.subunitsToUnit = subunitsToUnit;
+        this.isCrypto = isCrypto;
     }
 
     public BigDecimal getSubunitsToUnit() {
         return subunitsToUnit;
+    }
+
+    /**
+     * @return true if it's a Crypto currency like BTC, BCH, CLP
+     */
+    public boolean isCrypto() {
+        return isCrypto;
     }
 }

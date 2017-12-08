287c31b82359f4db93244881a82c3147b7432112
daplay <daplay@gmail.com>
2017-12-08 11:35:14 -0800

    - Amount now extends BigDecimal, and provides overloads for common operations: +, -, *, /

--- src/main/java/cl/daplay/jsurbtc/model/Amount.java
+++ src/main/java/cl/daplay/jsurbtc/model/Amount.java
@@ -9,27 +9,36 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Objects;
 
 import static java.lang.String.format;
 
 @JsonFormat(shape = JsonFormat.Shape.ARRAY)
 @JsonPropertyOrder({ "amount", "currency" })
-public class Amount implements Serializable, Comparable<Amount> {
+public class Amount extends BigDecimal implements Serializable {
 
     private static final long serialVersionUID = 2017_08_06;
 
+    @JsonProperty("currency")
     private final Currency currency;
+    @JsonProperty("amount")
     @JsonSerialize(using = BigDecimalToStringSerializer.class)
     private final BigDecimal amount;
 
     public Amount(Amount other) {
-        this.currency = other.currency;
-        this.amount = other.amount;
+        this(other.currency, other.amount);
     }
 
     @JsonCreator
     public Amount(@JsonProperty("currency") final Currency currency,
                   @JsonProperty("amount") final BigDecimal amount) {
+        super(amount.toBigInteger(), amount.scale());
+
+        Objects.requireNonNull(currency);
+        Objects.requireNonNull(amount);
+
         this.currency = currency;
         this.amount = amount;
     }
@@ -38,25 +47,177 @@ public class Amount implements Serializable, Comparable<Amount> {
         return currency;
     }
 
-    public BigDecimal getAmount() {
-        return amount;
+    public Amount add(Amount o) {
+        requireSameCurrency(o);
+        return new Amount(currency, super.add(o));
+    }
+
+    public Amount add(Amount o, MathContext mc) {
+        requireSameCurrency(o);
+        return new Amount(currency, super.add(o, mc));
+    }
+
+    public Amount subtract(Amount subtrahend) {
+        requireSameCurrency(subtrahend);
+        return new Amount(currency, super.subtract(subtrahend));
+    }
+
+    public Amount subtract(Amount subtrahend, MathContext mc) {
+        requireSameCurrency(subtrahend);
+        return new Amount(currency, super.subtract(subtrahend, mc));
+    }
+
+    public Amount multiply(Amount multiplicand) {
+        requireSameCurrency(multiplicand);
+        return new Amount(currency, super.multiply(multiplicand));
+    }
+
+    public Amount multiply(Amount multiplicand, MathContext mc) {
+        requireSameCurrency(multiplicand);
+        return new Amount(currency, super.multiply(multiplicand, mc));
+    }
+
+    public Amount divide(Amount divisor, int scale, int roundingMode) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.divide(divisor, scale, roundingMode));
+    }
+
+    public Amount divide(Amount divisor, int scale, RoundingMode roundingMode) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.divide(divisor, scale, roundingMode));
+    }
+
+    public Amount divide(Amount divisor, int roundingMode) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.divide(divisor, roundingMode));
+    }
+
+    public Amount divide(Amount divisor, RoundingMode roundingMode) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.divide(divisor, roundingMode));
+    }
+
+    public Amount divide(Amount divisor) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.divide(divisor));
+    }
+
+    public Amount divide(Amount divisor, MathContext mc) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.divide(divisor, mc));
+    }
+
+    public Amount remainder(Amount divisor) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.remainder(divisor));
+    }
+
+    public Amount remainder(Amount divisor, MathContext mc) {
+        requireSameCurrency(divisor);
+        return new Amount(currency, super.remainder(divisor, mc));
+    }
+
+    @Override
+    public Amount add(BigDecimal augend) {
+        return new Amount(currency, super.add(augend));
+    }
+
+    @Override
+    public Amount add(BigDecimal augend, MathContext mc) {
+        return new Amount(currency, super.add(augend, mc));
+    }
+
+    @Override
+    public Amount subtract(BigDecimal subtrahend) {
+        return new Amount(currency, super.subtract(subtrahend));
+    }
+
+    @Override
+    public Amount subtract(BigDecimal subtrahend, MathContext mc) {
+        return new Amount(currency, super.subtract(subtrahend, mc));
+    }
+
+    @Override
+    public Amount multiply(BigDecimal multiplicand) {
+        return new Amount(currency, super.multiply(multiplicand));
+    }
+
+    @Override
+    public Amount multiply(BigDecimal multiplicand, MathContext mc) {
+        return new Amount(currency, super.multiply(multiplicand, mc));
+    }
+
+    @Override
+    public Amount divide(BigDecimal divisor, int scale, int roundingMode) {
+        return new Amount(currency, super.divide(divisor, scale, roundingMode));
+    }
+
+    @Override
+    public Amount divide(BigDecimal divisor, int scale, RoundingMode roundingMode) {
+        return new Amount(currency, super.divide(divisor, scale, roundingMode));
+    }
+
+    @Override
+    public Amount divide(BigDecimal divisor, int roundingMode) {
+        return new Amount(currency, super.divide(divisor, roundingMode));
+    }
+
+    @Override
+    public Amount divide(BigDecimal divisor, RoundingMode roundingMode) {
+        return new Amount(currency, super.divide(divisor, roundingMode));
+    }
+
+    @Override
+    public Amount divide(BigDecimal divisor) {
+        return new Amount(currency, super.divide(divisor));
+    }
+
+    @Override
+    public Amount divide(BigDecimal divisor, MathContext mc) {
+        return new Amount(currency, super.divide(divisor, mc));
+    }
+
+    @Override
+    public Amount remainder(BigDecimal divisor) {
+        return new Amount(currency, super.remainder(divisor));
+    }
+
+    @Override
+    public Amount remainder(BigDecimal divisor, MathContext mc) {
+        return new Amount(currency, super.remainder(divisor, mc));
+    }
+
+    @Override
+    public Amount pow(int n) {
+        return new Amount(currency, super.pow(n));
+    }
+
+    @Override
+    public Amount pow(int n, MathContext mc) {
+        return new Amount(currency, super.pow(n, mc));
+    }
+
+    private void requireSameCurrency(Amount other) {
+        if (currency != other.currency) {
+            throw new IllegalArgumentException(format("Can't mix currencies: %s, %s", currency, other.currency));
+        }
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
 
-        Amount amount1 = (Amount) o;
+        Amount amount = (Amount) o;
 
-        if (currency != null ? !currency.equals(amount1.currency) : amount1.currency != null) return false;
-        return amount != null ? amount.equals(amount1.amount) : amount1.amount == null;
+        return currency == amount.currency;
     }
 
     @Override
     public int hashCode() {
-        int result = currency != null ? currency.hashCode() : 0;
-        result = 31 * result + (amount != null ? amount.hashCode() : 0);
+        int result = super.hashCode();
+        result = 31 * result + currency.hashCode();
         return result;
     }
 
@@ -64,24 +225,8 @@ public class Amount implements Serializable, Comparable<Amount> {
     public String toString() {
         return "Amount{" +
                 "currency=" + currency +
-                ", amount=" + amount +
+                ", amount=" + format("%.2f", amount) +
                 '}';
-    }
-
-    @Override
-    public int compareTo(Amount o) {
-        sameCurrencyCheck(o);
-        return this.getAmount().compareTo(o.getAmount());
-    }
-
-    private void sameCurrencyCheck(Amount amount) {
-        if (this.getCurrency() == amount.getCurrency()) {
-            return;
-        }
-
-        final String t = "Can't mix currencies: %s, %s";
-        final String m = format(t, this.getCurrency(), amount.getCurrency());
-        throw new IllegalArgumentException(m);
     }
 
 }

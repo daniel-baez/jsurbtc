2a6ebf0f29b4d5b7856eb547b8191db796e0d8ff
daplay <daplay@gmail.com>
2017-12-08 17:24:53 -0800

    - renames JSurbtcException.Error to JSurbtcException.Detail

--- src/main/java/cl/daplay/jsurbtc/JSurbtcException.java
+++ src/main/java/cl/daplay/jsurbtc/JSurbtcException.java
@@ -7,56 +7,67 @@ import static java.lang.String.format;
  */
 public final class JSurbtcException extends Exception {
 
-    public static class Error {
+    public static class Detail {
 
         public final String resource;
         public final String field;
         public final String code;
         public final String message;
 
-        public Error(final String resource, final String field, final String code, final String message) {
+        public Detail(final String resource, final String field, final String code, final String message) {
             this.resource = resource;
             this.field = field;
             this.code = code;
             this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Detail{");
+            sb.append("resource='").append(resource).append('\'');
+            sb.append(", field='").append(field).append('\'');
+            sb.append(", code='").append(code).append('\'');
+            sb.append(", message='").append(message).append('\'');
+            sb.append('}');
+            return sb.toString();
         }
     }
 
     public final int httpStatusCode;
     public final String message;
     public final String code;
-    public final Error[] errors;
+    public final Detail[] details;
 
     public JSurbtcException(String message) {
         super(message);
         this.httpStatusCode = 0;
         this.message = message;
         this.code = "";
-        this.errors = new Error[0];
+        this.details = new Detail[0];
     }
 
-    public JSurbtcException(final int httpStatusCode, final String message, final String code, final Error[] errors) {
+    public JSurbtcException(final int httpStatusCode, final String message, final String code, final Detail[] details) {
         super(message(httpStatusCode, message, code));
         this.httpStatusCode = httpStatusCode;
         this.message = message;
         this.code = code;
-        this.errors = errors;
+        this.details = details;
     }
 
-    public JSurbtcException(final int httpStatusCode, final Throwable cause, final String message, final String code, final Error[] errors) {
+    public JSurbtcException(final int httpStatusCode, final Throwable cause, final String message, final String code, final Detail[] details) {
         super(message(httpStatusCode, message, code), cause);
         this.httpStatusCode = httpStatusCode;
         this.message = message;
         this.code = code;
-        this.errors = errors;
+        this.details = details;
     }
 
-    public JSurbtcException(final int httpStatusCode, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace, final String message, final String code, final Error[] errors) {
+    public JSurbtcException(final int httpStatusCode, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace, final String message, final String code, final Detail[] details) {
         super(message(httpStatusCode, message, code), cause, enableSuppression, writableStackTrace);
         this.httpStatusCode = httpStatusCode;
         this.message = message;
         this.code = code;
-        this.errors = errors;
+        this.details = details;
     }
 
     private static String message(final int httpStatusCode, final String message, final String code) {

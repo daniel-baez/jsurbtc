1b1636a0f14e20f50e86cd3c58442ab9fa8090a6
daplay <daplay@gmail.com>
2018-02-18 19:59:50 -0800

    fix(JSurbtcException): improves JSurbtcException output, including details

--- src/main/java/cl/daplay/jsurbtc/model/JSurbtcException.java
+++ src/main/java/cl/daplay/jsurbtc/model/JSurbtcException.java
@@ -1,5 +1,8 @@
 package cl.daplay.jsurbtc.model;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static java.lang.String.format;
 
 /**
@@ -47,7 +50,7 @@ public final class JSurbtcException extends Exception {
     }
 
     public JSurbtcException(final int httpStatusCode, final String message, final String code, final Detail[] details) {
-        super(message(httpStatusCode, message, code));
+        super(message(httpStatusCode, message, code, details));
         this.httpStatusCode = httpStatusCode;
         this.message = message;
         this.code = code;
@@ -55,7 +58,7 @@ public final class JSurbtcException extends Exception {
     }
 
     public JSurbtcException(final int httpStatusCode, final Throwable cause, final String message, final String code, final Detail[] details) {
-        super(message(httpStatusCode, message, code), cause);
+        super(message(httpStatusCode, message, code, details), cause);
         this.httpStatusCode = httpStatusCode;
         this.message = message;
         this.code = code;
@@ -63,14 +66,21 @@ public final class JSurbtcException extends Exception {
     }
 
     public JSurbtcException(final int httpStatusCode, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace, final String message, final String code, final Detail[] details) {
-        super(message(httpStatusCode, message, code), cause, enableSuppression, writableStackTrace);
+        super(message(httpStatusCode, message, code, details), cause, enableSuppression, writableStackTrace);
         this.httpStatusCode = httpStatusCode;
         this.message = message;
         this.code = code;
         this.details = details;
     }
 
-    private static String message(final int httpStatusCode, final String message, final String code) {
-        return format("Surbtc request failed. http status code: %d,%n message: '%s',%n code: '%s'", httpStatusCode, message, code);
+    private static String message(final int httpStatusCode, final String message, final String code, final Detail[] details) {
+        String detailsString = Arrays.stream(details)
+                .map(Object::toString)
+                .map(str -> " - " + str)
+                .collect(Collectors.joining("\n"));
+
+        String template = "Surbtc request failed. %n HTTP Status Code: %d,%n Message: '%s',%n Code: '%s',%n Details:%n%s";
+
+        return format(template, httpStatusCode, message, code, detailsString);
     }
 }
